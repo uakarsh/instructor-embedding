231d955773aa97d6c11c6baaa9e96b560b28dc53
Akarsh <akarshupadhyayabc@gmail.com>
2023-06-27 17:18:43 +0530

    Update train.py

--- train.py
+++ train.py
@@ -533,7 +533,7 @@ def main():
             desc="Running tokenizer on train dataset",
         )
 
-    train_dataset.train_test_split(test_size = 0.1)  
+    train_dataset = train_dataset.train_test_split(test_size = 0.1)  
     print(train_dataset)
     print(f"Total training dataset: {len(train_dataset['train'])}")
     print(f"Total validation dataset: {len(train_dataset['test'])}")
